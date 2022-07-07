9c37333f0527613d2221241e213a944b700de3f3
Kim YeongRae <dudfo3728@gmail.com>
2022-07-07 21:23:34 +0900

    Create 2441 별찍기4.java
    백준 2441 별찍기4

---
+++ 2441 별찍기4.java
@@ -0,0 +1,30 @@
+https://yoloaeee.tistory.com/249
+
+===================================================
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=n; i>0; i--)
+		{
+			for(int j=n; j>i; j--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=i; j>0; j--)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
