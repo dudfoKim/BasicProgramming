8f212f65efde4a68364c51caa52302fef2c1da5c
Kim YeongRae <dudfo3728@gmail.com>
2022-09-16 10:37:13 +0900

    Create 25501 재귀의 귀재.java
    백준 25501 재귀의 귀재

---
+++ 25501 재귀의 귀재.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static int[] recursion(String s, int left, int right, int cnt)
+    {
+        if(left>=right)
+        {
+            return new int[]{1, cnt};
+        }
+        else if(s.charAt(left)!=s.charAt(right))
+        {
+            return new int[]{0, cnt};
+        }
+        else
+        {
+            return recursion(s, left+1, right-1, cnt+1);
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int[] result = new int[2];
+        String buffer = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<t; i++)
+        {
+            String input = sc.nextLine();
+            result = recursion(input, 0, input.length()-1, 1);
+            
+            for(int d : result)
+            {
+                sb.append(d + " ");
+            }
+            
+            sb.append("\n");
+        }
+        
+        System.out.println(sb.substring(0, sb.length()-1).toString());
+        sc.close();
+    }
+}
